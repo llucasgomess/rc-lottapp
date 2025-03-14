67d08cfbf5f40bc46cf69822c4e5ffd393834d5a
Lucas Gomes <llucas.gomes.dev@gmail.com>
2025-03-14 11:38:31 -0400

    :wrench: chore: add Eslint + Prettier setup , and format code setup

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,9 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native';
 
-export default function Index(){
-    return(
-        <View>
-            <Text>Ola mundo</Text>
-        </View>
-    )
+export default function Index() {
+  return (
+    <View>
+      <Text>Ola mundo</Text>
+    </View>
+  );
 }
